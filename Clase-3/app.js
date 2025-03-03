9f2fc21d83f8e0da08a5b7dabf39626f5bff41b9
Fracisco Acosta <rwilly203@gmail.com>
2025-03-03 17:56:00 -0600

    SOLUTION CORS
    SOLUTION CORS

--- Clase-3/app.js
+++ Clase-3/app.js
@@ -1,30 +1,52 @@
 const express = require('express'); // require --> commonJS
 const crypto = require('node:crypto');
 const movies = require('./movies.json');
+const cors = require('cors');
 const { validateMovie } = require('./schemas/moves');
 
 const app = express();
 
 app.use(express.json);
+app.use(cors({
+    origin: (origin, callback) => {
+        const ACCEPTED_ORIGINS = [
+            'http://localhost:3000',
+            'http://localhost:8000',
+            'http://localhost:8080',
+            'http://midu.dev'
+        ];
+
+        if (ACCEPTED_ORIGINS.includes(origin)) {
+            return callback(null, true);
+        }
+
+        if (!origin) {
+            return callback(null, true);
+        }
+        return callback(new Error('Not allowed by CORS'));
+    }
+}
+));
 app.disable('x-powered-by');
 
-const ACCEPTED_ORIGINS = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://localhost:8080',
-    'http://midu.dev'
-];
+// metodos normales: GET/HEAD/POST
+// metodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
+
+
 
 app.get('/', (req, res) => {
     res.json({message: 'Hola mundo'})
 });
 
 app.get('/movies', (req, res) => {
-    const origin = req.header('origin');
-    // cuando la peticion es del msimo ORIGIN
-    if( ACCEPTED_ORIGINS.includes(origin) || !origin) {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // * todos los origenes que esten permitidos
-    }
+    // const origin = req.header('origin');
+    // // cuando la peticion es del msimo ORIGIN
+    // if( ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    //     res.header('Access-Control-Allow-Origin', origin); // * todos los origenes que esten permitidos
+    // }
    
     const { genre } = req.query // trae valor en JSON
     if(genre) {
@@ -34,6 +56,21 @@ app.get('/movies', (req, res) => {
         return res.json(filtereMovies);
     }
  res.json(movies)
+})
+
+app.delete('/movies/:id', (req, res) => {
+    // const origin = req.header('origin');
+    // // cuando la peticion es del msimo ORIGIN
+    // if( ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    //     res.header('Access-Control-Allow-Origin', origin); // * todos los origenes que esten permitidos
+    // }
+
+    const { id } = req.params;
+    const movieIndex = movies.findIndex(movie => movie.id === id);
+    if ( movieIndex === -1 ) return res.status(404).json({ message: 'Movie not found' });
+    movies.splice(movieIndex, 1);
+    res.json(movies)
+
 })
 
 app.get('/movies/:id', (req, res) => {
@@ -81,6 +118,16 @@ app.patch('/movies/:id', (req, res) => {
      movies[movieIndex] = updateMovie;
 
      return res.json(updateMovie);
+})
+
+app.options('movies/:id', (req, res) => {
+    const origin = req.header('origin');
+    // cuando la peticion es del msimo ORIGIN
+    if( ACCEPTED_ORIGINS.includes(origin) || !origin) {
+        res.header('Access-Control-Allow-Origin', origin); // * todos los origenes que esten permitidos
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    }
+    res.send(200)
 })
 
 const PORT = process.env.PORT ?? 3000;
